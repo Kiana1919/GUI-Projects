f0abe308cbc35ee52f905e12f6689d153cb64cd9
Kiana1919 <79869430+Kiana1919@users.noreply.github.com>
2021-05-04 11:03:42 -0600

    Add files via upload

--- tkinkerTest.py
+++ tkinkerTest.py
@@ -2,9 +2,21 @@ from tkinter import *
 
 top = Tk()
 
-L1 = Label(top, text="Hello, world!")
+def results():
+    result = E1.get()
+    print(result)
+    print(type(result))
 
+# This is a Label widget
+L1 = Label(top, text="Hello, world!")
 L1.grid(column= 0, row= 1)
 
+#This is an Entry widget
+E1 = Entry(top, bd = 5)
+E1.grid(column= 0, row= 2)
+
+#This is a Button widget
+B1 = Button(text= "    Get Data    ", bg="red", command= results)
+B1.grid(column= 0, row= 3)
 
 top.mainloop()
